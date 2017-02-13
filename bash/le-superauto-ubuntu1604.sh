4092a6365702c193f4afaae889ec39294add0d5c
brunston poon <brunston@users.noreply.github.com>
2017-02-13 14:35:49 -0800

    Update le-superauto-ubuntu1604.sh

--- bash/le-superauto-ubuntu1604.sh
+++ bash/le-superauto-ubuntu1604.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo Using apt-get update
-`apt-get update`
+`sudo apt-get update`
 echo Installing letsencrypt
-`apt-get install letsencrypt python-letsencrypt-apache`
+`sudo apt-get install letsencrypt python-letsencrypt-apache`
 echo Getting ssl certificate
-`letsencrypt --apache -d $1`
+`sudo letsencrypt --apache -d $1`
 echo Adding renewal to crontab
-`(crontab -l ; echo "0 6 * * 1 /usr/bin/letsencrypt renew >> /var/log/ssl-renewal.log")| crontab -`
+`(sudo crontab -l ; echo "0 6 * * 1 /usr/bin/letsencrypt renew >> /var/log/ssl-renewal.log")| crontab -`
 echo If there are no errors, you should be set up with letsencrypt!
 
